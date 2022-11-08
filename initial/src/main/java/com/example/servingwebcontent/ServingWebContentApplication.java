7dcb40ce9566a57a9b30a88071834228c9ada3f5
Margarita Villaseñor <maggie.villasenor@hotmail.com>
2022-11-08 11:55:51 -0600

    add: create a web page with Spring MVC and Thymeleaf

--- initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServingWebContentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServingWebContentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
 
 }
